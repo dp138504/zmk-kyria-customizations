f961fb5c33d0c512d5a6f3e4dbd6abe92e17ea84
Dylan Pitts <60586225+dp138504@users.noreply.github.com>
2024-03-13 19:24:33 -0400

    mod: update modifier order based on keymap

--- boards/shields/kyria-custom-oled/widgets/modifiers.c
+++ boards/shields/kyria-custom-oled/widgets/modifiers.c
@@ -56,7 +56,7 @@ struct modifier_symbol ms_gui = {
 
 struct modifier_symbol *modifier_symbols[] = {
     // this order determines the order of the symbols
-    &ms_gui, &ms_alt, &ms_control, &ms_shift};
+    &ms_control, &ms_gui, &ms_shift, &ms_alt};
 
 #define NUM_SYMBOLS                                                            \
   (sizeof(modifier_symbols) / sizeof(struct modifier_symbol *))
